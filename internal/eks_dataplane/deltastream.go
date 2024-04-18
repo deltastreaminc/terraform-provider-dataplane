3c654b9066c996f8556c4a6a3b48282361faa8f0
Krishna Raman <kraman@deltastream.io>
2024-04-18 20:37:42 +0000

    restart flux after applying manifests

--- internal/eks_dataplane/deltastream.go
+++ internal/eks_dataplane/deltastream.go
@@ -8,10 +8,12 @@ import (
 	"context"
 	_ "embed"
 	"text/template"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+	appsv1 "k8s.io/api/apps/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -56,6 +58,23 @@ func InstallDeltaStream(ctx context.Context, cfg aws.Config, dp EKSDataplane, ku
 	})...)
 	if d.HasError() {
 		return
+	}
+
+	deployments := appsv1.DeploymentList{}
+	if err := kubeClient.List(ctx, &deployments, client.InNamespace("flux-system")); err != nil {
+		d.AddError("error listing flux-system deployments", err.Error())
+		return
+	}
+
+	for _, deployment := range deployments.Items {
+		if deployment.Spec.Template.Annotations == nil {
+			deployment.Spec.Template.Annotations = map[string]string{}
+		}
+		deployment.Spec.Template.Annotations["io.deltastream.tf-deltastream/restartedAt"] = time.Now().Format(time.RFC3339)
+		if err := kubeClient.Update(ctx, &deployment); err != nil {
+			d.AddError("error updating deployment "+deployment.Name, err.Error())
+			return
+		}
 	}
 
 	return
